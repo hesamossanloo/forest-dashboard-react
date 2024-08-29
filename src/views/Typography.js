08dc83508d6b4b7763c2b537fd9c3fa9d3ce89a9
Hesam Ossanloo (Personal) <hesam@ossanloo.com>
2024-08-29 16:53:38 +0200

    chore: Remove file header comments

--- src/views/Typography.js
+++ src/views/Typography.js
@@ -1,25 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
-/*!
 
-=========================================================
-* Black Dashboard React v1.2.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React from 'react';
-
-// reactstrap components
-import { Card, CardHeader, CardBody, CardTitle, Row, Col } from 'reactstrap';
+import { Card, CardBody, CardHeader, CardTitle, Col, Row } from 'reactstrap';
 
 function Typography() {
   return (
